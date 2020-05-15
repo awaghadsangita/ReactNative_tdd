96961356df64f059ebdfad9fff5067a52d9e065d
sangita avghad <sangita.awaghad@gmail.com>
2020-05-15 06:05:43 -0700

    ADDED:test case for snapshot testing

--- tests/components/smoke.spec.js
+++ tests/components/smoke.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native'; 
 import { shallow } from 'enzyme';
-
-  describe('Text', () => {
-   it('renders text', () => {
-     const wrapper = shallow(<Text>Hello, world!</Text>);
-     expect(wrapper.text()).toEqual('Hello, world!');
+import App from '../../App';
+import renderer from 'react-test-renderer';
+describe('Text', () => {
+    it('renders text', () => {
+        const wrapper = shallow(<Text>Hello, world!</Text>);
+        expect(wrapper.text()).toEqual('Hello, world!');
    });
+   it('snapshottesting',()=>{
+       const snap=renderer.create(<App/>).toJSON();
+       expect(snap).toMatchSnapshot();
+   })
  });
+
