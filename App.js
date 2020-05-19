cd2d4ad07b5b11906257687d0a09026962b8df1a
sangita avghad <sangita.awaghad@gmail.com>
2020-05-19 04:17:10 -0700

    REFACTORED:updated the layout

--- App.js
+++ App.js
@@ -1,36 +1,88 @@
-import React  from 'react';
-import { Text, View,StyleSheet } from 'react-native';
+import React from 'react';
+import { View, StyleSheet,Button,Text } from 'react-native';
 
-export default class App extends React.Component {  
-  render(){
-  return (
-    <View style={styles.container}>
-      <View style={styles.result}>
-        <Text style={styles.resultText}>01</Text>
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.result}><Text style={styles.resultText}>11*11</Text></View>
+        <View style={styles.calculation}><Text style={styles.calculationText}>121</Text></View>
+        <View style={styles.button}>
+          <View style={styles.numbers}>
+            <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+            </View>
+            <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+            </View>
+            <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+            </View>
+            <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+            </View>
+          </View>
+          <View style={styles.operations}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+          </View>
+        </View>
       </View>
-      <View style={styles.keypad}></View>
-      
-    </View>
     );
-}  
+  }
 }
-const styles= StyleSheet.create({
-    container:{
-      flex:1,
-      paddingTop:40
-    },
-    result:{
-      flex:2,
-      backgroundColor:"black"
-    },
-    keypad:{
-      flex:4,
-      backgroundColor:"gray"  
-    },
-    resultText:{
-     fontSize:48,
-     color:"white",
-     paddingTop:75,
-     textAlign:"right"
-     }
-     })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  row:{
+    flexDirection:"row",
+    flex:1,
+    justifyContent:"space-around",
+    alignItems:"stretch",  
+  },
+  result: {
+    flex: 2,
+    backgroundColor: 'red',
+  },
+  resultText:{
+    fontSize:30,
+    color:"black",
+    textAlign:"right",
+    paddingTop:80
+  },
+  calculation:{
+    flex:1,
+    backgroundColor:"white",
+  },
+  calculationText:{
+    fontSize:40,
+    color:"gray",
+    textAlign:"right"
+  },
+  button:{
+    flex:7,
+    flexDirection:"row"
+  },
+  numbers:{
+    flex:3,
+    backgroundColor:"yellow"
+  },
+  operations:{
+    flex:1,
+    justifyContent:"space-around",
+    backgroundColor:"black"
+  }  
+});
+
