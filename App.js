6ad9be1c8820d6feed18202171278fe3103a76e1
sangita avghad <sangita.awaghad@gmail.com>
2020-05-19 06:51:17 -0700

    ADDED:function for deleting last character of operend

--- App.js
+++ App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import { View, StyleSheet,Button,Text,TouchableOpacity } from 'react-native';
+import { View, StyleSheet,Text,TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      resultText1:""
+    }
+  }
+  pressButton(text){
+    if(text=="="){
+        return this.calculatResult();
+    }
+    this.setState({
+      resultText1:this.state.resultText1+text
+    })
+  }
+  calculatResult(){
+
+  }
+  operation(operation){
+    switch(operation){
+      case 'D':{
+          let text=this.state.resultText1.split('');
+          text.pop();
+          this.setState({resultText1:text.join('')})
+      }
+    }
+  }
   render() {
     let rows=[]
     let numbers=[[1,2,3],[4,5,6],[7,8,9],[0,".","="]]
     for(let i=0;i<4;i++){
         let row=[]
       for(let j=0;j<4;j++){
-        row.push(<TouchableOpacity style={styles.btn}><Text style={styles.btnText}>{numbers[i][j]}</Text></TouchableOpacity>)
+        row.push(<TouchableOpacity 
+                  onPress={()=>this.buttonPress(numbers[i][j])}
+                  style={styles.btn}>
+              <Text style={styles.btnText}>{numbers[i][j]}</Text>
+            </TouchableOpacity>)
       }
       rows.push(<View style={styles.row}>{row}</View>)
     }
-    let ops=["/","*","-","+"];
+    let ops=["D","/","*","-","+"];
     let opsColumn=[]
-    for(let i=0;i<4;i++){
-      opsColumn.push(<TouchableOpacity style={styles.btn}><Text style={styles.white}>{ops[i]}</Text></TouchableOpacity>)
+    for(let i=0;i<5;i++){
+      opsColumn.push(<TouchableOpacity
+                        onPress={()=>this.operation(ops[i])} 
+                        style={styles.btn}>
+                        <Text style={styles.white}>{ops[i]}</Text>
+                    </TouchableOpacity>)
     }
     return (
       <View style={styles.container}>
-        <View style={styles.result}><Text style={styles.resultText}>11*11</Text></View>
+        <View style={styles.result}><Text style={styles.resultText}>{this.state.resultText1}</Text></View>
         <View style={styles.calculation}><Text style={styles.calculationText}>121</Text></View>
         <View style={styles.button}>
           <View style={styles.numbers}>
